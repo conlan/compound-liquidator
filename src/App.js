23331f0234bd59393cce862ed2b3d10b5482a68a
Conlan Rios <conlan@gmail.com>
2018-12-02 17:35:38 -0800

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Web3Provider } from 'react-web3';
+
+class App extends Component {
+	constructor() {
+		super(); 
+	}
+
+  render() {
+    return (
+      <div className="App">
+      	hi
+      </div>
+    );
+  }
+}
+
+export default App;
